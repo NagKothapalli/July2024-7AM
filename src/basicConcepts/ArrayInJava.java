7c6d1083920b8ac4b7dfdd013839d45bf683f9a1
NagKothapalli <nag022@gmail.com>
2024-07-19 10:55:00 +0530

    July19th code pushed

--- src/basicConcepts/ArrayInJava.java
+++ src/basicConcepts/ArrayInJava.java
@@ -1,5 +1,100 @@
 package basicConcepts;
 
-public class ArrayInJava {
+import org.junit.Test;
 
+public class ArrayInJava
+{
+	
+	
+	//Array : Is a group of memory locations with similar data type
+	
+	//Modifier  DataType Name = value ;
+	
+	public int stdMarks = 34;
+	
+	//Modifier DataType[]  Name =  { values } ;
+	
+	public int[]  studentsMarks  = {56,67,45,78,32,78,67};
+	
+	// Declare , Assign , Read , Write , size
+	@Test
+	public void fixedIntegerArray()
+	{
+		int[]  marks  = {56,67,45,78,32,18,37};  //7 [6]
+		
+		int len = marks.length;
+		System.out.println("Length of the array :" + len);
+		int m1 = marks[0];
+		for(int i=0;i<len;i++)
+		{
+			int m = marks[i];  // i = 0 1 2 3  .... 6   // Reading the values
+			System.out.println("Marks :" + m); //56 67 45 78 .... 37
+		}
+		
+		System.out.println("-----------------------------------");
+		for(int i=0;i<len;i++)
+		{
+			marks[i]  = 222+i;   // Writing values
+			System.out.println("i value :"+ i);
+			System.out.println("Marks after :" + marks[i]);
+		}
+		
+		//System.out.println("Marks from 8th location :" + marks[8]);
+		
+		System.out.println("End of program");
+		
+	}
+	
+	
+	@Test
+	public void dynamicIntegerArray()
+	{
+		//int[]  marks  = {56,67,45,78,32,18,37};  //7 [6]
+		int[] marks = new int[20];
+		int len = marks.length;
+		System.out.println("Length of the array :" + len);
+		int m1 = marks[0];
+		for(int i=0;i<len;i++)
+		{
+			int m = marks[i];  // i = 0 1 2 3  .... 6   // Reading the values
+			System.out.println("Marks :" + m); //56 67 45 78 .... 37
+		}
+		
+		System.out.println("-----------------------------------");
+		for(int i=0;i<len;i++)
+		{
+			marks[i]  = 222+i;   // Writing values
+			System.out.println("i value :"+ i);
+			System.out.println("Marks after :" + marks[i]);
+		}
+		
+		System.out.println("Marks from 8th location :" + marks[21]);
+		
+		System.out.println("End of program");
+		
+	}
+	
+	//VIP customer   - ArrayList
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/basicConcepts/GmailWithJunit.java
+++ src/basicConcepts/GmailWithJunit.java
@@ -5,28 +5,79 @@ import org.junit.Test;
 public class GmailWithJunit 
 {
 	@Test   // Java Method must be public , non static , no return type , no arguments
-	public void composeAnEmail()
+	public void composeAnEmail()   // Test Case
 	{
 		//body
 		System.out.println("TestCase : ComposeAnEmail");
+		launchApplication();
+		loginApplication();
+
+		//launch-15 , login-15 , compose , send , logout , close
 	}
 	@Test
-	public void replyToEmail()
+	public void replyToEmail()  // Test Case   // Ram
 	{
 		//body
 		System.out.println("TestCase : ReplyToEmail");
+		//launch , login , open , reply , logout , close
+		launchApplication();
+		loginApplication();
 	}
 	@Test
-	public void forwardAnEmail()
+	public void forwardAnEmail() // Test Case    Raj
 	{
 		//body
 		System.out.println("TestCase : ForwardAnEmail");
+		//launch , login , open , forward , logout , close
+		launchApplication();
+		loginApplication();
 	}
 	@Test
-	public void deleteEmail()
+	public void deleteEmail() // Test Case    Tej
 	{
 		//body
 		System.out.println("TestCase : DeleteEmail");
+		//launch , login , open , delete , logout , close
+		launchApplication();
+		loginApplication();
 	}
+	
+	//******************************************************************
+	
+	
+	public void launchApplication()   
+	{
+		//write the required code to do a launch 15 or 20 lines of code
+		//open browser , check internet , maximize , call gmail url , see page loaded , check the title of the page
+		System.out.println("Launch Application");
+		
+	}
+	
+	public void loginApplication()
+	{
+		System.out.println("Login To Application");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }

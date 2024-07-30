2e4cab1201f2eb4059dab7911e132862b5a2ccec
NagKothapalli <nag022@gmail.com>
2024-07-30 09:42:02 +0530

    July30th code pushed

--- src/seleniumLearning/GmailAutomation.java
+++ src/seleniumLearning/GmailAutomation.java
@@ -1,6 +1,7 @@
 package seleniumLearning;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,15 +9,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GmailAutomation
 {
 	//Google chrome browser
+	public static String collegeName = "JNTU";
+	int stdNum ; // default value zero
+	String stdName ; // null
+	ChromeDriver driver ; // null  // Chrome driver constructor will open an empty google chrome browser
 	
-	ChromeDriver driver = new ChromeDriver(); //1234 // Chrome driver constructor will open an empty google chrome browser
+	public GmailAutomation() // ByDeault a Constructor of that class will be executed
+	{
+		System.out.println("Inside Constructor : Clear the DataBase - Delete All Temp Records");
+		stdNum = 44;
+		stdName = "Ram";
+		driver = new ChromeDriver(); //1234
+	}
 	// get(url);
 	//validation : expected == actual 
 	String expected = "Gmail";
 	@Test
 	public void launchGmailApplication()
-	{
-		System.out.println("Launch Gmail Application");
+	{		
+		System.out.println("Inside : Launch Gmail Application");
 		driver.get("https://gmail.com");
 		String actual = driver.getTitle();
 		System.out.println("Actual title :"+ actual);
@@ -38,15 +49,60 @@ public class GmailAutomation
 		
 		System.out.println("End of the Test Case");
 	}
-	@Test
+	
+	//Execution Flow : Instance variables  -> Constructor  -> Called Function
+	
+	@Test  // The method tagged with Before annotation will be executed before each test case 
 	public void loginToGmailApplication()
 	{
+		System.out.println("Inside : Login To Application");
 		launchGmailApplication();
 		//enter user name
 		driver.findElement(By.name("identifier")).sendKeys("nag022@gmail.com");
+		
+		driver.findElements(By.className("VfPpkd-vQzf8d")).get(1).click();
+	}
+	@Test
+	public void createAnAccount()
+	{
+		System.out.println("Test Case : Create An Account");
+		launchGmailApplication();
+		driver.findElements(By.className("VfPpkd-vQzf8d")).get(2).click();
+		
+	}
+	//Execution Flow : Instance variables  -> Constructor  -> Called Function
+	@Test
+	public void composeEmail()
+	{
+		System.out.println("Inside : Test Case : Compose Email");
+		//login
+		//loginToGmailApplication();
+		
+		
 	}
 	
+	@Test
+	public void replyToEmail()
+	{
+		//login
+		//loginToGmailApplication();
+	}
 	
+	@Test
+	public void forwardEmail()
+	{
+		//login
+		//loginToGmailApplication();
+	}
+	
+	@Test
+	public void deleteEmail()
+	{
+		//login
+		//loginToGmailApplication();
+	}
+	
+	//if we have 40 test cases all of them need login function 
 
 }
 
